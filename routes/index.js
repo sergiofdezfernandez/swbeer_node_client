88e9439a2c84902f6562c41d2dff3292b80a6732
sergio <sergiofdezfernandez@gmail.com>
2021-02-14 20:17:40 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var soap = require('soap')
+
+var client = null;
+const url = 'http://localhost:8080/WS_Unit03_Example1_war_exploded/soapws/calculator?wsdl';
+soap.createClient(url, function (err, soapClient) {
+    client = soapClient;
+});
+
+router.get('/', function (req, res, next) {
+    res.render('index', {result:{}});
+});
+
+router.post('/', function (req, res, next) {
+    const args = {arg0: req.body.term1, arg1: req.body.term2}
+    client.add(args, function (err, result) {
+        console.log(result)
+        return res.render('index', {result})
+    });
+});
+module.exports = router;
