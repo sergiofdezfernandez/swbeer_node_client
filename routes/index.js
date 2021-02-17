d2214476e957e9caeb2f67f9b25b5f87747ef970
sergio <sergiofdezfernandez@gmail.com>
2021-02-17 20:54:32 +0100

    transale api with amazon sdk.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var soap = require('soap')
+var AWS = require('aws-sdk');
+const config = require('config');
+
+AWS.config.update({region: 'us-east-2'})
+const accessKeyId = config.get('AWS.accessKeyId');
+const secretAccessKey = config.get('AWS.secretAccessKey');
+const region = config.get('AWS.region');
+
+AWS.config.update(
+    {
+        accessKeyId,
+        secretAccessKey,
+        region
+    }
+);
 
 var client = null;
-const url = 'http://localhost:8080/WS_Unit03_Example1_war_exploded/soapws/calculator?wsdl';
+const url = config.get("SOAP-SERVER.url");
 
 soap.createClient(url, function (err, soapClient) {
     client = soapClient;
@@ -18,6 +33,26 @@ router.post('/', function (req, res, next) {
     client.add(args, function (err, result) {
         console.log(result)
         return res.render('index', {result})
+    });
+});
+router.get('/translate', function (req, res, next) {
+    res.render('test', {text:""});
+});
+
+router.post('/translate', function (req, res, next) {
+    var translate = new AWS.Translate();
+    var params = {
+        Text: req.body.text,
+        SourceLanguageCode: "en",
+        TargetLanguageCode: "es"
+    };
+    translate.translateText(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        }
+        if (data) {
+            res.render('test', {text: data.TranslatedText});
+        }
     });
 });
 module.exports = router;
